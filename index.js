da1ff674a9f6a55e1ddbce278c4af0953a5572c0
Ryuske Hideaki Sato <88846546+Dragonrhs@users.noreply.github.com>
2024-03-16 12:18:17 -0300

    Aula_2_02_03

---
+++ index.js
@@ -0,0 +1,80 @@
+//const, let e var
+
+// var idade = 18 //var mantem-se por retrocompatibilidade 
+// console.log(`Oi,${nome}`) //dupla passagem do js
+// if (idade >= 18){
+//     var nome ="João"   /// efeito de içamento (hoist) como se vc tivesse delcarado ela para fora do if 
+//     console.log(`Parabéns, ${nome}! , Você pode dirigir.`)
+// }
+// console.log(`Até logo, ${nome}`)
+
+//var linguagem = "Javascript"
+//console.log(`apendendo ${linguagem}`)
+//var linguagem = "Java"                 //houve uma redefiniçao da variavel
+//console.log(`apendendo ${linguagem}`)
+
+//var a=2
+//a=3
+
+
+/*function f(){
+let a = 2
+const b = 3
+}*/
+
+///let idade = 19 //pode reatribuir a variavel
+///idade = 20
+//idade = "abc"
+//idade.comer()
+
+//let idade: number //typscrypt(sistema de tipo estatico) 
+//idade.falar() //esta protegido idade n fala
+
+//const nome = "José"   //nao pode reatribuir 
+//const sobrenome ="Silva"
+//const nomeFinal = "Mendes"
+
+//vc decide entre let e const vendo o menor privilegio(pincipio do menor privilegio) a principio vc quer const
+
+//nao pode (reatribuiçao de constantes)
+//nome = "Ana"
+
+//no java
+//int a =2
+// a="Ana"
+//a.falar()
+//String b = "abc"
+
+
+//coerçao implicita
+
+// const n1 = 2
+// const n2 =`3`
+// const n3 = n1 + n2
+// console.log(n3) //23
+
+//coerçao explicita
+
+// const n4 = n1 + Number(n2)
+// console.log(n4) //5
+
+//comparaçao
+// console.log(2 == 2) //true
+// console.log(2 == "2") //true
+// console.log(2 === "2") //false
+// console.log(2 === 2) //true
+//!= !==
+
+//let a = new Girafa()
+//let b = new Macaco()
+
+//let p1 = new Pessoa(`V`)
+//let p2 = new Pessoa(`V`)
+
+// console.log(1==[1]) //true
+// console.log(1==[1,2]) //false
+//console.log(null==null) //true
+//console.log(null==undefined) //true
+//console.log([]==false) //true
+//console.log([]==[]) //false
+
