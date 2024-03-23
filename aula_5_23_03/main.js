e3d2950501f5e2bb3419ddd48162955452d0de50
dragonrhs <ryuske2009@hotmail.com>
2024-03-23 08:24:06 -0300

    <refactor>[]: comentado funçoes de call back

--- aula_5_23_03/main.js
+++ aula_5_23_03/main.js
@@ -1,30 +1,34 @@
 
-const executa = (f) =>{f()};   //funçao call back
+// const executa = (f) =>{f()};   //funçao call back
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const abrirArquivo = (nomeArquivo) => {
+// const abrirArquivo = (nomeArquivo) => {
 
-    //const a = () =>{}; //primeira classe é quando vc pode tratar uma funçao como um valor manipulavel
+//     //const a = () =>{}; //primeira classe é quando vc pode tratar uma funçao como um valor manipulavel
 
-    const exibirConteudo = function(erro, conteudo){
+//     const exibirConteudo = function(erro, conteudo){
     
-        if(erro)
-            console.log(`Deu Erro: ${erro}`);
-        else{
+//         if(erro)
+//             console.log(`Deu Erro: ${erro}`);
+//         else{
 
-            console.log(conteudo.toString());
-            const dobro = Number(conteudo.toString()) * 2;
-            const finalizar = function(erro){
-                console.log("Salvou o dobro com sucesso");
+//             console.log(conteudo.toString());
+//             const dobro = Number(conteudo.toString()) * 2;
+//             const finalizar = function(erro){
+//                 console.log("Salvou o dobro com sucesso");
 
-            }
+//             }
 
-            fs.writeFile('dobro.txt', dobro, finalizar)  
-             }
+//             fs.writeFile('dobro.txt', dobro, finalizar)  
+//              }
 
-}
+// }
 
-fs.readFile(nomeArquivo, exibirConteudo); 
-}
-abrirarquivo("arquivo.txt");  //sequencia de execuçao de codigo 7 vai para 23 codigo raduken
+// fs.readFile(nomeArquivo, exibirConteudo); 
+// }
+// abrirarquivo("arquivo.txt");  //sequencia de execuçao de codigo 7 vai para 23 codigo raduken
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
