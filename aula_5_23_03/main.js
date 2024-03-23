a1ebbaf7b3903c7c35cdf7dc7a626fb62d7194af
dragonrhs <ryuske2009@hotmail.com>
2024-03-23 08:19:56 -0300

    <feat>[]: implementaçao do dobro

--- aula_5_23_03/main.js
+++ aula_5_23_03/main.js
@@ -1,23 +1,30 @@
 
 const executa = (f) =>{f()};   //funçao call back
 
-
 const fs = require('fs');
 
 const abrirArquivo = (nomeArquivo) => {
 
-    //const a = () =>{}; //primeira classe é qunaod vc pode tratar uma funçao como um valor manipulavel
+    //const a = () =>{}; //primeira classe é quando vc pode tratar uma funçao como um valor manipulavel
 
     const exibirConteudo = function(erro, conteudo){
     
         if(erro)
             console.log(`Deu Erro: ${erro}`);
-        else
+        else{
+
             console.log(conteudo.toString());
-        
-    
-    }
-    fs.readFile(nomeArquivo, exibirConteudo); 
+            const dobro = Number(conteudo.toString()) * 2;
+            const finalizar = function(erro){
+                console.log("Salvou o dobro com sucesso");
+
+            }
+
+            fs.writeFile('dobro.txt', dobro, finalizar)  
+             }
 
 }
-abriraqrquivo("arquivo.txt");  //sequencia de execuçao de codigo 7 vai para 23 codigo raduken
+
+fs.readFile(nomeArquivo, exibirConteudo); 
+}
+abrirarquivo("arquivo.txt");  //sequencia de execuçao de codigo 7 vai para 23 codigo raduken
