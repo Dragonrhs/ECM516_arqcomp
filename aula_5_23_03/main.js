36636cc3bd0493f8d92d8ba667bf9e2b1501309b
dragonrhs <ryuske2009@hotmail.com>
2024-03-23 08:07:12 -0300

    <feat>[]: funçao call back e codigo "raduken"

--- aula_5_23_03/main.js
+++ aula_5_23_03/main.js
@@ -1,2 +1,23 @@
 
 const executa = (f) =>{f()};   //funçao call back
+
+
+const fs = require('fs');
+
+const abrirArquivo = (nomeArquivo) => {
+
+    //const a = () =>{}; //primeira classe é qunaod vc pode tratar uma funçao como um valor manipulavel
+
+    const exibirConteudo = function(erro, conteudo){
+    
+        if(erro)
+            console.log(`Deu Erro: ${erro}`);
+        else
+            console.log(conteudo.toString());
+        
+    
+    }
+    fs.readFile(nomeArquivo, exibirConteudo); 
+
+}
+abriraqrquivo("arquivo.txt");  //sequencia de execuçao de codigo 7 vai para 23 codigo raduken
